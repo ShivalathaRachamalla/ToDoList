6ef478249b44130d20ae3cd8aa5d073d21472469
Shivalatha Rachamalla <shivalatha.r@gmail.com>
2020-03-13 11:56:52 +0100

    code formatting

--- src/Task.java
+++ src/Task.java
@@ -1,6 +1,5 @@
-
-/***
- * Prompt class has the details of the Taskname, ProjectName, Status and date
+/*
+ Prompt class has the details of the Taskname, ProjectName, Status and date
  */
 
 import java.util.Date;
@@ -11,79 +10,50 @@ public class Task {
     private String projectName;
     private String status;
     private Date taskDate;
-    private Date dueDate;
 
-
-    public Task(String taskName, String projectName, String status, Date taskDate)
-    {
+    public Task(String taskName, String projectName, String status, Date taskDate) {
         this.taskName    = taskName;
         this.projectName = projectName;
         this.status      = status;
         this.taskDate    = taskDate;
     }
 
-/**
- * @get Task name
- */
-    public String getTaskName()
-    {
+    // get Task name
+    public String getTaskName() {
         return taskName;
     }
 
-/**
- * @get Task description
- */
-    public String getProjectName()
-    {
+    // get Project name
+    public String getProjectName() {
         return projectName;
     }
 
-/**
- * @get Task status
- */
-    public String getStatus()
-    {
+    // get task status
+    public String getStatus() {
         return status;
     }
 
-/**
- * @get Task date
- */
-    public Date getTaskDate()
-    {
+    // get Task date
+    public Date getTaskDate() {
         return taskDate;
     }
 
-/**
- * @get Due date
- */
-    public Date getDueDate() {
-        return dueDate;
-    }
-/**
- * @set Task name
- */
+    // set Task name
     public void setTaskName(String taskName) {
         this.taskName = taskName;
     }
 
-/**
- * @set Task description
- */
+    // set Project name
     public void setProjectName(String projectName) {
         this.projectName = projectName;
     }
 
-/**
- * @set Task status
- */
+    // set Task status
     public void setStatus(String status) {
         this.status = status;
     }
 
-/**
- * @set Task date
- */
+    // set Task date
     public void setTaskDate(Date taskDate) {
         this.taskDate = taskDate;
     }
