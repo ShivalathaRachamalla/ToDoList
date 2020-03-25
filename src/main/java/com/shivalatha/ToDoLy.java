67a839ea92d85e6e686dfb3f534c23df320cacc8
Shivalatha Rachamalla <shivalatha.r@gmail.com>
2020-03-25 20:43:10 +0100

    Final changes made to the source code

---
+++ src/main/java/com/shivalatha/ToDoLy.java
@@ -0,0 +1,17 @@
+package com.shivalatha;
+
+/**
+ *  This is the Main class of Task Application
+ */
+
+public class ToDoLy {
+
+    public static void main(String[] args) throws Exception {
+
+        Prompt.getInstance().displayOption();
+
+    }
+
+}
+
+
